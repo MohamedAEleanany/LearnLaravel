1d0abee98c4e16f56f46853d4f4c76da1b2bb827
Mohamed Eleanany <mohamedeleanany7@gmail.com>
2025-08-21 18:13:05 +0300

    Main Files

---
+++ routes/web.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+Route::get('/', function () {
+    return view('welcome');
+});
+*/
+
+
+//          address  , clouse(call back function)
+Route::get('/' , function(){
+    dd("Hello Mohamed");
+});
