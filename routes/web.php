2aa94ef0eb4ec3070e3d05e560287247956c5b10
Mohamed Eleanany <mohamedeleanany7@gmail.com>
2025-08-22 11:53:54 +0300

    Invokeable Controller

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\SingleController;
 use App\Http\Controllers\TestController;
+use App\Http\Controllers\UserController;
+//use App\Http\Controllers\users\UserController;
 use Illuminate\Support\Facades\Route;
 
 
@@ -11,9 +14,15 @@ Route::get('/', function () {
 /*
 1- get  => push data with Request with URL
 2- post => data in Request Body not in URL 
+3-command to list the routes of the project => php artisan route:list or php artisan r:l
+4- Naming Convertion for 
+    1-Controller => for module Users,Posts,etc.. make for it Controller (UserController , PostController)
+
+5-Single Action Controller invokable => php artisan make:controller SingleController --invokable
+    description : like Constructor run automatic without calling in route
 */
 
-//command to list the routes of the project => php artisan route:list or php artisan r:l
+
 
 
 //          address  , clouse(call back function)
@@ -29,9 +38,12 @@ Route::redirect('/here', 'https://www.google.com');
 //Route::get('/printName/{name?}', [TestController::class ,'printName'])->name('printMyNameRoute');
 
 //  Routes Groups
-Route::controller(TestController::class)->name('test.')->group(function(){
-   Route::get('/ahmed', 'ahmed'); 
-   Route::get('/printName/{name?}', 'printName')->name('printMyNameRoute');
-
+Route::controller(TestController::class)->name('test.')->group(function () {
+    Route::get('/ahmed', 'ahmed');
+    Route::get('/printName/{name?}', 'printName')->name('printMyNameRoute');
 });
 
+Route::get('/user', [UserController::class, 'index']);
+
+//invokable Calling 
+Route::get('/singlecontroller' , SingleController::class);
